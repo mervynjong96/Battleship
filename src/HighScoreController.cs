0f49c2b8c4d7323b65348d1a7d19fa562db57f37
mervynjong96 <4335023@students.swinburne.edu.my>
2016-10-19 20:39:11 +0800

    Fix inappropriate text display of high score text-box

--- src/HighScoreController.cs
+++ src/HighScoreController.cs
@@ -181,7 +181,7 @@ internal static class HighScoreController
 
 			int x = SCORES_LEFT + SwinGame.TextWidth(GameResources.GameFont("Courier"), "Name: ");
 
-			SwinGame.StartReadingText(Color.White, NAME_WIDTH, GameResources.GameFont("Courier"), x, ENTRY_TOP);
+			SwinGame.StartReadingText(Color.White, NAME_WIDTH + 1, GameResources.GameFont("Courier"), x, ENTRY_TOP);
 
 			//Read the text from the user
 			while (SwinGame.ReadingText())
